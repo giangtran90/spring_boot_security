86839ffc0b083a893bfc7e5b56b6fb5f2f8950f4
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-09-24 10:35:05 +0900

    Khoi tao du an

---
+++ spring-boot-security/src/test/java/com/hg/client/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
